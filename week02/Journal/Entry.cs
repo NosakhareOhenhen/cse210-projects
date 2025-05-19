11a63cfda0eaff8d4bebca5384f98ba9bb8d3b86
NosakhareOhenhen <acenosa@gmail.com>
2025-05-19 16:48:40 +0100

    Entry.cs

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Entry
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public string Date { get; set; }
+
+    public Entry(string prompt, string response, string date)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = date;
+    }
+
+    public override string ToString()
+    {
+        return $"Date: {Date}\nPrompt: {Prompt}\nResponse: {Response}\n";
+    }
+
+    public string ToFileString()
+    {
+        return $"{Date}|{Prompt}|{Response}";
+    }
+
+    public static Entry FromFileString(string fileString)
+    {
+        string[] parts = fileString.Split('|');
+        return new Entry(parts[1], parts[2], parts[0]);
+    }
+}
