df55f22197c857a6a4354ff20bd340033183e279
NosakhareOhenhen <acenosa@gmail.com>
2025-05-19 16:45:47 +0100

    journal 10

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,51 @@
-using System;
+usinusing System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        bool running = true;
+
+        Console.WriteLine("Welcome to the Journal Program 1.0");
+
+        while (running)
+        {
+            Console.WriteLine("\nPlease select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    journal.WriteNewEntry();
+                    break;
+                case "2":
+                    journal.DisplayJournal();
+                    break;
+                case "3":
+                    Console.Write("Enter filename to load: ");
+                    string loadFile = Console.ReadLine();
+                    journal.LoadJournal(loadFile);
+                    break;
+                case "4":
+                    Console.Write("Enter filename to save: ");
+                    string saveFile = Console.ReadLine();
+                    journal.SaveJournal(saveFile);
+                    break;
+                case "5":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
 }
