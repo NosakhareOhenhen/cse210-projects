d95c3265d241eefe4cd077b557c33cf1de016f15
NosakhareOhenhen <acenosa@gmail.com>
2025-05-24 21:04:47 +0100

    Fraction

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction(1, 3);
+
+        
+        f1.SetTop(2);
+        f1.SetBottom(3);
+        Console.WriteLine($"Top: {f1.GetTop()}, Bottom: {f1.GetBottom()}");
+
+        
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        
+        Console.WriteLine($"{f1.GetFractionString()}");
+        Console.WriteLine($"{f1.GetDecimalValue()}");
+        Console.WriteLine($"{f2.GetFractionString()}");
+        Console.WriteLine($"{f2.GetDecimalValue()}");
+        Console.WriteLine($"{f3.GetFractionString()}");
+        Console.WriteLine($"{f3.GetDecimalValue()}");
+        Console.WriteLine($"{f4.GetFractionString()}");
+        Console.WriteLine($"{f4.GetDecimalValue()}");
     }
 }
